497cdaa6ccd444d157de6bf09f6d5ce84a25cc04
xuexiao <xuexiao@belink.com>
2023-09-15 22:17:12 +0800

    Spring Security 关闭DEMO

--- olap-app/src/main/java/org/cloud/xue/OLAPApplication.java
+++ olap-app/src/main/java/org/cloud/xue/OLAPApplication.java
@@ -1,7 +1,9 @@
 package org.cloud.xue;
 
+import com.sun.org.apache.xerces.internal.parsers.SecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 /**
  * @ClassName OLAPApplication
@@ -10,6 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2023年09月14日 10:07:18
  * @Version 1.0
  **/
+//exclude属性值为SecurityAutoConfiguration.class，则关闭Spring Security校验
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+
 @SpringBootApplication
 public class OLAPApplication {
     public static void main(String[] args) {
