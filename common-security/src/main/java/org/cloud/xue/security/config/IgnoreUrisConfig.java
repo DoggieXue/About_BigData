31c6e749c7928bc7d248ee5fe8777b2eecb042ec
xuexiao <xuexiao@belink.com>
2023-09-17 21:28:01 +0800

    引入Spring Security配置

---
+++ common-security/src/main/java/org/cloud/xue/security/config/IgnoreUrisConfig.java
@@ -0,0 +1,20 @@
+package org.cloud.xue.security.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 用于配置白名单资源路径
+ */
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "secure.ignored")
+public class IgnoreUrisConfig {
+
+    private List<String> urls = new ArrayList<>();
+
+}
